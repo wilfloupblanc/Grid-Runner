0c8d15da48cdcc04df683b6f8e2147afee8bfea5
wilfloupblanc <zaugjulien@gmail.com>
2026-01-11 20:04:07 +0100

    V1 terminée

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'grid-runner-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/assets/styles/index.css',
+    '/script.js',
+    '/grid.js',
+    '/images/gridrunner.png',
+    '/images/mur.jpeg'
+];
+
+// Installation
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Activation
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Récupération
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
